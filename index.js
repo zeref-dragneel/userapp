671de1e22cee6c37ffe4825ecd33b00b023991ba
Sharath Mohan <30803741+zeref-dragneel@users.noreply.github.com>
2020-05-28 22:01:54 +0530

    testadded

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const port = process.env.PORT || 3000
 const path  = require('path')
 app.use(express.static(__dirname+'/public'))
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname)))
+app.get("/api/home",(req,res)=>res.json({"msg":"hello form server"}))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
